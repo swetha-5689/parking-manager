b38637914dbfb6b5205c345bf079fa34fb3696c6
Swetha Angara <swetha.angara5689@gmail.com>
2020-02-23 23:32:06 -0500

    Added react front end

---
+++ manager/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Nav from "./Nav";
+import Home from "./Home";
+import Statistics from "./Statistics";
+import Reservations from "./Reservations";
+import Overview from "./Overview";
+import { MemoryRouter } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <MemoryRouter>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Route path="/home" component={Home} />
+          <Route path="/statistics" component={Statistics} />
+          <Route path="/reservations" component={Reservations} />
+          <Route path="/overview" component={Overview} />
+        </div>
+      </Router>
+    </MemoryRouter>
+  );
+}
+
+export default App;
---
+++ manager/src/Overview.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function Overview() {
+    return (
+        <p></p>
+    );
+}
+export default Overview;
