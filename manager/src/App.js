b678273cf7e2d4b4c97198f50b135131cfed09d4
Swetha Angara <swetha.angara5689@gmail.com>
2020-03-15 15:20:50 -0400

    Co-authored-by: jeffreys@live.com <jeffreys@live.com> Co-authored-by: anir9 <rahim.aniqa@gmail.com>

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"; 
 import Nav from "./Nav";
 import Home from "./Home";
 import Statistics from "./Statistics";
--- manager/src/Home.js
+++ manager/src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function Home() {
     return (
-        <h1>Hello!</h1>
+        <h1>Hello! Welcome to SmarkPark!</h1>
     );
 }
 export default Home;
