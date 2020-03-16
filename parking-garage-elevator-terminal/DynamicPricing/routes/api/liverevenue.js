a05a7ef44de3020cff2a5f0b68cea587e3af8b9c
Charles Owen <47432133+clo64@users.noreply.github.com>
2020-03-15 21:33:15 -0400

    Add files via upload

---
+++ parking-garage-elevator-terminal/DynamicPricing/routes/api/liverevenue.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const PriceModelClass = require('../../services/PriceModelClass.js');
+
+//Object with connection to the Live Price Model for calculations
+const LivePriceModel = require('../../models/LivePriceModel');
+
+//@route Get api/liverevenue
+//@desc Generates a first array containing the hourly
+//      revenue based on the dynamic price
+//      Denerates a second array containing the hourly
+//      revenue based on the static price
+//      Send both arrays to the caller in JSON format
+//      "live_revenue" : "arr"
+//      "static_revenue": "arr"
+
+router.get('/', (req, res) => {
+
+    LivePriceModel.find({model_name: "Live Model"})
+                  .then(pricemodel => {
+                    //Retrieve the setup values from the database
+                    let model_name = pricemodel[0].model_name;
+                    let base_rate = pricemodel[0].base_rate;
+                    let min_thresh = pricemodel[0].min_thresh;
+                    let max_thresh = pricemodel[0].max_thresh;
+                    let base_rate_mult = pricemodel[0].base_rate_mult;
+                    let occupancy_precent = pricemodel[0].occupancy_percent;
+                    //this.hours_to_park = hours_to_park; Not needed
+                    let total_spots = pricemodel[0].total_spots;
+                    //Create a new PriceModel Class object, this is where we use all the 
+                    //price model data retreived from mongoDb
+                    let myModel = new PriceModelClass(model_name, base_rate, min_thresh, max_thresh, base_rate_mult, occupancy_precent, total_spots);
+                    //Calculate hourlRates array required for the customer price function
+                    let hourlyRates = myModel.calculateHourlyRateArray();
+                    //Calculate the daily revenue with dynamic rates
+                    let revenueArray = myModel.calculateDailyRevenue(hourlyRates);
+                    //Get the hourly rates based on the current base_rate       
+                    let hourlyRatesStatic = myModel.calculateHourlyRateArrayStatic();
+                    //Calculate the daily revenue with dynamic rates
+                    let revenueArrayStatic = myModel.calculateDailyRevenue(hourlyRatesStatic);
+                    //Place both arrays in a JSON friendly object
+                    let returnBothArrays = {live_revenue: revenueArray, static_revenue: revenueArrayStatic};
+                    res.json([returnBothArrays]);
+                });
+});
+
+module.exports = router;
