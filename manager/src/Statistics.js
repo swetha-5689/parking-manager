831b3758e33284515ec2a1c98bab11d4022276d1
anir9 <48391406+anir9@users.noreply.github.com>
2020-03-17 18:19:49 -0400

    Add files via upload

--- manager/src/Statistics.js
+++ manager/src/Statistics.js
@@ -10,145 +10,19 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import Pagination from "react-bootstrap/Pagination";
+import Graph from "./components/Graph";
+//import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function Statistics() {
   return (
-    <div>
-      {/*Start*/}
-      <h1>Parking Garage Statistics</h1>
-      <Container>
-        {/*Data Table*/}
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Month</th>
-              <th>Overstays</th>
-              <th>No-Shows</th>
-              <th>Availabilities</th>
-              <th>Occupancies</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>6</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>7</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>8</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>9</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>10</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>11</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td>12</td>
-              <td>100</td>
-              <td>10</td>
-              <td>200</td>
-              <td>50</td>
-            </tr>
-          </tbody>
-        </Table>
-      </Container>
-      {/* Data Selection: OPTION 1*/}
-      <Container>
-        <Form inline>
-          <ButtonToolbar
-            className="mb-3"
-            aria-label="Toolbar with Button groups"
-          >
-            <ButtonGroup className="mr-2" aria-label="First group">
-              <Button>Daily</Button>
-              <Button>Weekly</Button>
-              <Button>Monthly</Button>
-              <DropdownButton
-                as={ButtonGroup}
-                title="By Year"
-                id="bg-nested-dropdown"
-              >
-                <Dropdown.Item eventKey="1">2020</Dropdown.Item>
-                <Dropdown.Item eventKey="2">2021</Dropdown.Item>
-              </DropdownButton>
-            </ButtonGroup>
-            <InputGroup>
-              <FormControl
-                type="text"
-                placeholder="Enter Data Range"
-                aria-label="Enter Data Range"
-                aria-describedby="btnGroupAddon"
-              />
-            </InputGroup>
-            {/* Search Button */}
-            <Button variant="outline-success">Search</Button>
-          </ButtonToolbar>
-        </Form>
-      </Container>
+    <div style={{ justifyContent: "center" }}>
+      {/*Header*/}
+      <h1 style={{ justifyContent: "center", alignItems: "center" }}>
+        Parking Garage Statistics
+      </h1>
+      <Graph></Graph>
+
       {/* Data Selection: OPTION 2*/}
       <Container>
         <Form>
