82847a68199bef29da4fe7a60f810ac89af1889e
Brian Ogbebor <bbrian125@gmail.com>
2020-03-20 20:12:56 -0400

    Changing URI and Port Number -Brian

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.use(cors());
 app.use(express.json());
