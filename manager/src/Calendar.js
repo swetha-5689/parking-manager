f1a3dcd39c9f5bef7ee9ba1d8e8fbca2071fdbd1
Aniqa <rahim.aniqa@gmail.com>
2020-04-02 17:54:13 -0400

    Calendar

--- manager/src/Calendar.js
+++ manager/src/Calendar.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import { MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import "./index.css";
-import { CarouselIndicators } from "reactstrap";
 class App extends Component {
     constructor(props) {
         super(props);
